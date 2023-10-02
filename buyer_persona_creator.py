20ab36d81e2265fe45dc0a3dd1e9342aad940aad
Daniel LaPushin <dlapushin@gmail.com>
2023-10-02 14:17:32 -0700

    Update buyer_persona_creator.py

--- buyer_persona_creator.py
+++ buyer_persona_creator.py
@@ -81,7 +81,7 @@ def contacts_import(approach):
             st.subheader("Simple Approach")
             st.write(
                 """
-                Upload a file with **Contact ID, Contact Job Title, Contact CreatedDate** at a minimum.
+                Upload a file with **Contact Job Title, Contact CreatedDate, ContactID (optional)** at a minimum.
                 **No personal information (PII) is required**.
                 """
             )
